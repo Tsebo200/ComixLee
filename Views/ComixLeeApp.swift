b6c49819d486ca390170f508c0f3702e13831a08
Tsebo Rams <200200@virtualwindow.co.za>
2022-09-21 19:46:12 +0200

    Initial commit with Xcode

---
+++ Views/ComixLeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComixLeeApp.swift
+//  ComixLee
+//
+//  Created by Tsebo Ramonyalioa on 2022/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ComixLeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
