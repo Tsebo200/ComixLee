24a0b162e4c15f6233f1bb4c715f04d8ab030cd1
Tsebo Rams <200200@virtualwindow.co.za>
2022-10-06 11:10:36 +0200

    Process Mark One
    This is where I ended up

--- ViewModel/HomeViewModel.swift
+++ ViewModel/HomeViewModel.swift
@@ -6,7 +6,20 @@
 //
 
 import SwiftUI
+import Combine
 
 class HomeViewModel: ObservableObject {
     @Published var searchQuery = ""
+    
+    //Combine Framework Search barr...
+    
+    // used to cancel the search publisher when eveer we need....
+    let searchCancellabel: AnyCancellable? = nil
+    
+    init(){
+        // since SwiftUI uses @publish so its a publisher....
+        // so we dont need to explicityly define publisher...
+//        searchCancellabel = $searchquery 5:08
+    }
+    
 }
