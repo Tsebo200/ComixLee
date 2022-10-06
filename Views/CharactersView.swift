4579491323b2e80b8437842c47de774dc61f1f16
Tsebo Rams <200200@virtualwindow.co.za>
2022-10-06 10:58:39 +0200

    Search Bar implemented
    Just need to make it navigate now or show the comics/characters

--- Views/CharactersView.swift
+++ Views/CharactersView.swift
@@ -10,15 +10,29 @@ import SwiftUI
 struct CharactersView: View {
     @EnvironmentObject var homeData: HomeViewModel
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false, content:{
-            VStack(spacing: 15){
-                
-                // This is the Search Bar..
-                HStack(spacing: 10){
-                    Image(systemName: "magnifyingglass")
+        // Navigation View
+        NavigationView{
+            ScrollView(.vertical, showsIndicators: false, content:{
+                VStack(spacing: 15){
+                    
+                    // This is the Search Bar..
+                    HStack(spacing: 10){
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        
+                        TextField("Search Character", text: $homeData.searchQuery)
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal)
+                    .background(Color.white)
+                    
+                    .shadow(color: Color.black.opacity(0.06), radius: 5, x: 5, y: 5)
+                    .shadow(color: Color.black.opacity(0.06), radius: 5, x: 5, y: -5)
                 }
-            }
-        })
+                .padding()
+            })
+            .navigationTitle("Marvel")
+        }
     }
 }
 
