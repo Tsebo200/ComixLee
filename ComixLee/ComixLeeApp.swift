60ded6e47e988f3454c1eae48df0cc5681af3d66
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-02-11 00:46:56 +0200

    Initial Commit

---
+++ ComixLee/ComixLeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComixLeeApp.swift
+//  ComixLee
+//
+//  Created by Tsebo Ramonyalioa on 2025/02/11.
+//
+
+import SwiftUI
+
+@main
+struct ComixLeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComixLeeTests/ComixLeeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ComixLeeTests.swift
+//  ComixLeeTests
+//
+//  Created by Tsebo Ramonyalioa on 2025/02/11.
+//
+
+import Testing
+@testable import ComixLee
+
+struct ComixLeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
